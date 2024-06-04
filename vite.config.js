c63ab191b6e2ed25f54d9d9c9339e8248d832598
Nikogenia <info@nikogenia.de>
2024-06-04 20:35:05 +0200

    Vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { resolve } from 'path';
+import { resolve, relative, extname } from 'path';
+import { glob } from 'glob';
 import handlebars from 'vite-plugin-handlebars';
+
+let htmlFiles = await glob('src/**/*.html', { ignore: 'src/components/**' })
+let input = Object.fromEntries(htmlFiles.map(file => [relative('src', file.slice(0, file.length - extname(file).length)), resolve(__dirname, file)]))
 
 export default {
     root: "./src",
     build: {
         outDir: "../build",
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: input,
+        },
     },
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/components')
