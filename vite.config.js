bac11f6cea1436b952acfc21360826e3bab1770f
Nikogenia <info@nikogenia.de>
2024-06-04 18:23:58 +0200

    Homepage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
 export default {
     root: "./src",
     build: {
         outDir: "../build",
         emptyOutDir: true
-    }
+    },
+    plugins: [handlebars({
+        partialDirectory: resolve(__dirname, 'src/components')
+    })],
 }
